2a35f53cdbd9b4e72d39f79858c9502cfc1636b8
Wonmin Jung <wonmin82@gmail.com>
2016-12-28 21:44:57 +0900

    Remove unnecessary sudo

--- wonmin82-trusty/ubuntu-trusty-docker-setup.sh
+++ wonmin82-trusty/ubuntu-trusty-docker-setup.sh
@@ -129,9 +129,9 @@ retry()
 
 pre_process()
 {
-	echo "debconf debconf/frontend select Noninteractive" | sudo debconf-set-selections
+	echo "debconf debconf/frontend select Noninteractive" | debconf-set-selections
 
-	sudo rm -f /etc/apt/apt.conf.d/docker-clean
+	rm -f /etc/apt/apt.conf.d/docker-clean
 
 	user="$(getent passwd 1000 | cut -d: -f1)"
 	home="$(getent passwd 1000 | cut -d: -f6)"
@@ -148,71 +148,71 @@ pre_process()
 
 install_prerequisites()
 {
-	retry sudo aptitude update
-	retry sudo aptitude -y --with-recommends --download-only install apt-transport-https ca-certificates tasksel
-	sudo aptitude -y --with-recommends install apt-transport-https ca-certificates tasksel
+	retry aptitude update
+	retry aptitude -y --with-recommends --download-only install apt-transport-https ca-certificates tasksel
+	aptitude -y --with-recommends install apt-transport-https ca-certificates tasksel
 }
 
 add_ppa()
 {
 	# oracle java
-	sudo add-apt-repository ppa:webupd8team/java < /dev/null
+	add-apt-repository ppa:webupd8team/java < /dev/null
 
 	# mono
-	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-	echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+	echo "deb http://download.mono-project.com/repo/debian wheezy main" | tee /etc/apt/sources.list.d/mono-xamarin.list
 
-	retry sudo aptitude update
+	retry aptitude update
 }
 
 install_java()
 {
 	ORACLE_JAVA_PKG_PREFIX="oracle-java8"
-	retry sudo aptitude -y -d install ${ORACLE_JAVA_PKG_PREFIX}-installer ${ORACLE_JAVA_PKG_PREFIX}-set-default ${ORACLE_JAVA_PKG_PREFIX}-unlimited-jce-policy
+	retry aptitude -y -d install ${ORACLE_JAVA_PKG_PREFIX}-installer ${ORACLE_JAVA_PKG_PREFIX}-set-default ${ORACLE_JAVA_PKG_PREFIX}-unlimited-jce-policy
 	lastStatus=256
 	until [[ ${lastStatus} == 0 ]]; do
-		sudo aptitude -y purge ${ORACLE_JAVA_PKG_PREFIX}-installer ${ORACLE_JAVA_PKG_PREFIX}-set-default ${ORACLE_JAVA_PKG_PREFIX}-unlimited-jce-policy
-		echo ${ORACLE_JAVA_PKG_PREFIX}-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-		sudo aptitude -y install ${ORACLE_JAVA_PKG_PREFIX}-installer ${ORACLE_JAVA_PKG_PREFIX}-set-default ${ORACLE_JAVA_PKG_PREFIX}-unlimited-jce-policy
+		aptitude -y purge ${ORACLE_JAVA_PKG_PREFIX}-installer ${ORACLE_JAVA_PKG_PREFIX}-set-default ${ORACLE_JAVA_PKG_PREFIX}-unlimited-jce-policy
+		echo ${ORACLE_JAVA_PKG_PREFIX}-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+		aptitude -y install ${ORACLE_JAVA_PKG_PREFIX}-installer ${ORACLE_JAVA_PKG_PREFIX}-set-default ${ORACLE_JAVA_PKG_PREFIX}-unlimited-jce-policy
 		lastStatus=$?
 	done
 }
 
 fetch_all()
 {
-	aptitude_fetch_command="retry sudo aptitude -y --with-recommends --download-only install"
+	aptitude_fetch_command="retry aptitude -y --with-recommends --download-only install"
 	for task in "${list_tasks_to_be_installed[@]}"; do
 		list_pkg=($(tasksel --task-packages ${task}))
 		aptitude_fetch_command="${aptitude_fetch_command} ${list_pkg[@]}"
 		eval $aptitude_fetch_command
 	done
 
-	aptitude_fetch_command="retry sudo aptitude -y --with-recommends --download-only install"
+	aptitude_fetch_command="retry aptitude -y --with-recommends --download-only install"
 	aptitude_fetch_command="${aptitude_fetch_command} ${list_pkgs_to_be_installed[@]}"
 	eval $aptitude_fetch_command
 }
 
 install_all()
 {
-	aptitude_install_command="sudo aptitude -y --with-recommends install"
+	aptitude_install_command="aptitude -y --with-recommends install"
 	for task in "${list_tasks_to_be_installed[@]}"; do
 		list_pkg=($(tasksel --task-packages ${task}))
 		aptitude_install_command="${aptitude_install_command} ${list_pkg[@]}"
 		eval $aptitude_install_command
 	done
 
-	aptitude_remove_command="sudo aptitude -y purge"
+	aptitude_remove_command="aptitude -y purge"
 	aptitude_remove_command="${aptitude_remove_command} ${list_pkgs_to_be_uninstalled[@]}"
 	eval $aptitude_remove_command
 
-	aptitude_install_command="sudo aptitude -y --with-recommends install"
+	aptitude_install_command="aptitude -y --with-recommends install"
 	aptitude_install_command="${aptitude_install_command} ${list_pkgs_to_be_installed[@]}"
 	eval $aptitude_install_command
 }
 
 post_process()
 {
-	echo "debconf debconf/frontend select Dialog" | sudo debconf-set-selections
+	echo "debconf debconf/frontend select Dialog" | debconf-set-selections
 
 	user="$(getent passwd 1000 | cut -d: -f1)"
 	home="$(getent passwd 1000 | cut -d: -f6)"
