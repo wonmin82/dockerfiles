3018f445cbd03b961b640b63eaa3c090cddf1c22
Wonmin Jung <wonmin82@gmail.com>
2016-12-28 21:46:31 +0900

    Apply retry function to apt-key commands

--- wonmin82-trusty/ubuntu-trusty-docker-setup.sh
+++ wonmin82-trusty/ubuntu-trusty-docker-setup.sh
@@ -160,7 +160,7 @@ add_ppa()
 	add-apt-repository ppa:webupd8team/java < /dev/null
 
 	# mono
-	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+	retry apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 	echo "deb http://download.mono-project.com/repo/debian wheezy main" | tee /etc/apt/sources.list.d/mono-xamarin.list
 
 	retry aptitude update
