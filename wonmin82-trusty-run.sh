9189afef4fa62411b92d2f2fd7a50e8f740ba7ff
Wonmin Jung <wonmin82@gmail.com>
2016-12-30 23:03:10 +0900

    Merge build and run scripts

--- wonmin82-trusty-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e -x
-
-docker run --detach --publish=20022:22 --hostname=wonmin82-trusty-docker --name=wonmin82-trusty-docker wonmin82-trusty
--- wonmin82-xenial-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e -x
-
-docker run --detach --publish=10022:22 --hostname=wonmin82-xenial-docker --name=wonmin82-xenial-docker wonmin82-xenial
