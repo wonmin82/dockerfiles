52a84d0fb6e1037afe34c84df3bd9a394245c357
Wonmin Jung <wonmin82@gmail.com>
2017-05-12 18:29:35 +0900

    Adjust sshd port numbers to add ubuntu zesty docker

--- wonmin82-trusty-build.sh
+++ wonmin82-trusty-build.sh
@@ -3,5 +3,5 @@
 set -e -x
 
 docker build -t wonmin82-trusty $PWD/wonmin82-trusty
-docker run --detach --publish=30022:22 --hostname=wonmin82-trusty-docker --name=wonmin82-trusty-docker wonmin82-trusty
+docker run --detach --publish=40022:22 --hostname=wonmin82-trusty-docker --name=wonmin82-trusty-docker wonmin82-trusty
 docker stop wonmin82-trusty-docker
