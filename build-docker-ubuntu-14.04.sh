6cf1894ca0663d8c4374a6f60ad02b0c027facee
Wonmin Jung <wonmin82@gmail.com>
2019-03-05 16:04:56 +0900

    Fix build script

--- build-docker-ubuntu-14.04.sh
+++ build-docker-ubuntu-14.04.sh
@@ -3,5 +3,5 @@
 set -e -x
 
 docker build -t docker-ubuntu-14.04 $PWD/docker-ubuntu-14.04
-docker run --detach --publish=21404:22 --hostname=docker-ubuntu-14.04 --name=docker-ubuntu-14.04 ubuntu-14.04
+docker run --detach --publish=21404:22 --hostname=docker-ubuntu-trusty --name=docker-ubuntu-14.04 docker-ubuntu-14.04
 docker stop docker-ubuntu-14.04
--- build-docker-ubuntu-16.04.sh
+++ build-docker-ubuntu-16.04.sh
@@ -3,6 +3,6 @@
 set -e -x
 
 docker build -t docker-ubuntu-16.04 $PWD/docker-ubuntu-16.04
-docker run --detach --publish=21604:22 --hostname=docker-ubuntu-16.04 --name=docker-ubuntu-16.04 ubuntu-16.04
+docker run --detach --publish=21604:22 --hostname=docker-ubuntu-xenial --name=docker-ubuntu-16.04 docker-ubuntu-16.04
 docker stop docker-ubuntu-16.04
 
