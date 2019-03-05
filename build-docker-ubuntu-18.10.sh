d49e526be577807f379736dea8b09fb19eb820bc
Wonmin Jung <wonmin82@gmail.com>
2019-03-06 08:01:00 +0900

    Add ubuntu 18.10 docker setup script

---
+++ build-docker-ubuntu-18.10.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e -x
+
+docker build -t docker-ubuntu-18.10 $PWD/docker-ubuntu-18.10
+docker run --detach --publish=21810:22 --hostname=docker-ubuntu-cosmic --name=docker-ubuntu-18.10 docker-ubuntu-18.10
+docker stop docker-ubuntu-18.10
+
