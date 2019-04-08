cda31e0707256094d5494151768c46768475a1d8
Wonmin Jung <wonmin82@gmail.com>
2019-04-08 23:32:14 +0900

    Support ssh key

--- build-docker-ubuntu-14.04.sh
+++ build-docker-ubuntu-14.04.sh
@@ -2,19 +2,21 @@
 
 set -e -x
 
-docker build                        \
-	-t docker-ubuntu-14.04          \
-	--build-arg user=${USER}        \
+docker build                                           \
+	-t docker-ubuntu-14.04                             \
+	--build-arg user=${USER}                           \
+	--build-arg ssh_prv_key="$(cat ~/.ssh/id_rsa)"     \
+	--build-arg ssh_pub_key="$(cat ~/.ssh/id_rsa.pub)" \
 	$PWD/docker-ubuntu-14.04
-docker run                          \
-	--detach                        \
-	--publish=21404:22              \
-	--hostname=docker-ubuntu-trusty \
-	--name=docker-ubuntu-14.04      \
+docker run                                             \
+	--detach                                           \
+	--publish=21404:22                                 \
+	--hostname=docker-ubuntu-trusty                    \
+	--name=docker-ubuntu-14.04                         \
 	docker-ubuntu-14.04
-docker stop                         \
+docker stop                                            \
 	docker-ubuntu-14.04
-docker commit                       \
-	-m "Initial Ubuntu 14.04"       \
-	docker-ubuntu-14.04             \
+docker commit                                          \
+	-m "Initial Ubuntu 14.04"                          \
+	docker-ubuntu-14.04                                \
 	docker-ubuntu-14.04:initial
--- build-docker-ubuntu-16.04.sh
+++ build-docker-ubuntu-16.04.sh
@@ -2,19 +2,21 @@
 
 set -e -x
 
-docker build                        \
-	-t docker-ubuntu-16.04          \
-	--build-arg user=${USER}        \
+docker build                                           \
+	-t docker-ubuntu-16.04                             \
+	--build-arg user=${USER}                           \
+	--build-arg ssh_prv_key="$(cat ~/.ssh/id_rsa)"     \
+	--build-arg ssh_pub_key="$(cat ~/.ssh/id_rsa.pub)" \
 	$PWD/docker-ubuntu-16.04
-docker run                          \
-	--detach                        \
-	--publish=21604:22              \
-	--hostname=docker-ubuntu-xenial \
-	--name=docker-ubuntu-16.04      \
+docker run                                             \
+	--detach                                           \
+	--publish=21604:22                                 \
+	--hostname=docker-ubuntu-xenial                    \
+	--name=docker-ubuntu-16.04                         \
 	docker-ubuntu-16.04
-docker stop                         \
+docker stop                                            \
 	docker-ubuntu-16.04
-docker commit                       \
-	-m "Initial Ubuntu 16.04"       \
-	docker-ubuntu-16.04             \
+docker commit                                          \
+	-m "Initial Ubuntu 16.04"                          \
+	docker-ubuntu-16.04                                \
 	docker-ubuntu-16.04:initial
