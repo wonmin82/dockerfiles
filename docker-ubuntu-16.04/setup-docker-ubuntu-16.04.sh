783da4635cc915779692df9d6a7eeff6bfe13819
Wonmin Jung <wonmin82@gmail.com>
2021-05-16 16:11:16 +0900

    Fix script code wrapping

--- docker-ubuntu-16.04/setup-docker-ubuntu-16.04.sh
+++ docker-ubuntu-16.04/setup-docker-ubuntu-16.04.sh
@@ -245,10 +245,7 @@ add_repo() {
 	retry apt-key adv \
 		--keyserver hkp://keyserver.ubuntu.com:80 \
 		--recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-	echo "deb \
-		https://download.mono-project.com/repo/ubuntu \
-		stable-xenial \
-		main" |
+	echo "deb https://download.mono-project.com/repo/ubuntu stable-xenial main" |
 		tee /etc/apt/sources.list.d/mono-official-stable.list
 
 	add-apt-repository ppa:ultradvorka/ppa </dev/null
--- docker-ubuntu-20.10/setup-docker-ubuntu-20.10.sh
+++ docker-ubuntu-20.10/setup-docker-ubuntu-20.10.sh
@@ -291,10 +291,7 @@ add_repo() {
 			--keyserver hkp://keyserver.ubuntu.com:80 \
 			--recv-keys 52B59B1571A79DBC054901C0F6BC817356A3D45E
 		add-apt-repository --yes --no-update \
-			"deb \
-			http://ppa.launchpad.net/longsleep/golang-backports/ubuntu \
-			focal \
-			main" \
+			"deb http://ppa.launchpad.net/longsleep/golang-backports/ubuntu focal main" \
 			</dev/null
 	fi
 
@@ -303,10 +300,7 @@ add_repo() {
 	retry apt-key adv \
 		--keyserver hkp://keyserver.ubuntu.com:80 \
 		--recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-	echo "deb \
-		https://download.mono-project.com/repo/ubuntu \
-		stable-focal \
-		main" |
+	echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" |
 		tee /etc/apt/sources.list.d/mono-official-stable.list
 
 	# hstr
@@ -320,13 +314,9 @@ add_repo() {
 			--keyserver hkp://keyserver.ubuntu.com:80 \
 			--recv-keys 1E841C1E5C04D97ABFF8FCB63A9508A2CC6FC1EB
 		DISTRO="$(lsb_release -s -c)"
-		echo "deb http://ppa.launchpad.net/ultradvorka/ppa/ubuntu \
-			${DISTRO} \
-			main" |
+		echo "deb http://ppa.launchpad.net/ultradvorka/ppa/ubuntu ${DISTRO} main" |
 			tee /etc/apt/sources.list.d/ultradvorka.list
-		echo "deb-src http://ppa.launchpad.net/ultradvorka/ppa/ubuntu \
-			${DISTRO} \
-			main" |
+		echo "deb-src http://ppa.launchpad.net/ultradvorka/ppa/ubuntu ${DISTRO} main" |
 			tee -a /etc/apt/sources.list.d/ultradvorka.list
 	fi
 
