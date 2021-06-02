33a2e6382aa5e6d1602c4e1885f0be0151a0774c
Wonmin Jung <wonmin82@gmail.com>
2021-06-02 21:51:59 +0900

    Update support for configuration files

--- build-docker-ubuntu-18.04.sh
+++ build-docker-ubuntu-18.04.sh
@@ -4,13 +4,22 @@ set -e +x
 
 ssh_port=14022
 
+unset ssh_prv_key
+unset ssh_pub_key
+unset gitconfig
+
+[ -f ~/.ssh/id_rsa ] && ssh_prv_key=$(cat ~/.ssh/id_rsa)
+[ -f ~/.ssh/id_rsa.pub ] && ssh_pub_key=$(cat ~/.ssh/id_rsa.pub)
+[ -f ~/.gitconfig ] && gitconfig=$(cat ~/.gitconfig)
+
 docker build \
 	-t docker-ubuntu-18.04 \
 	--build-arg user="${USER}" \
 	--build-arg uid="$(id -u)" \
 	--build-arg gid="$(id -g)" \
-	--build-arg ssh_prv_key="$(cat ~/.ssh/id_rsa)" \
-	--build-arg ssh_pub_key="$(cat ~/.ssh/id_rsa.pub)" \
+	--build-arg ssh_prv_key="${ssh_prv_key}" \
+	--build-arg ssh_pub_key="${ssh_pub_key}" \
+	--build-arg gitconfig="${gitconfig}" \
 	$PWD/docker-ubuntu-18.04
 docker run \
 	--detach \
